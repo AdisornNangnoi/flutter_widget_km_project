8ac4b5a674382ced88e858a9f26e83f04e09f147
s6552410001@sau.ac.th <s6552410001@sau.ac.th>
2024-03-07 20:21:09 +0700

    1

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_km_project/views/km_bnb_03_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(
+    MainUI()
+  );
+}
+
+class MainUI extends StatefulWidget {
+  const MainUI({super.key});
+
+  @override
+  State<MainUI> createState() => _MainUIState();
+}
+
+class _MainUIState extends State<MainUI> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: KmBnb03UI(),
+      theme: ThemeData(
+          textTheme: GoogleFonts.kanitTextTheme(
+        Theme.of(context).textTheme,
+      )),
+    );
+  }
+}
